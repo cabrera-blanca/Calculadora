e187cf71a63aa7583b6f0849fa46484d8cf2d098
Lucia Cabrera <cabrera.blanca@hotmail.com>
2022-01-27 16:26:50 -0300

    upload

---
+++ main.js
@@ -0,0 +1,30 @@
+const displayValorAnterior = document.getElementById('valor-anterior');
+const displayValorActual = document.getElementById('valor-actual');
+const botonesNumeros = document.querySelectorAll('.numero');
+const botonesOperadores = document.querySelectorAll('.operador');
+
+const display = new Display (displayValorActual, displayValorAnterior);
+
+botonesNumeros.forEach(boton =>{
+    boton.addEventListener('click',() => {
+        display.agregarNumero(boton.innerHTML)
+    });
+}); 
+
+
+botonesOperadores.forEach(boton =>{
+    boton.addEventListener('click', () => display.computar(boton.value))
+});
+
+
+/*COLOR PICKER*/
+
+window.addEventListener("load", () => {
+    const colorItems = document.querySelectorAll('.color-item');
+    colorItems.forEach(item =>{
+        item.addEventListener('click', function(){
+            const idSelected = this.id;
+            document.body.className = idSelected;
+        })
+    })
+})
