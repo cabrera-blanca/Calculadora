9a9db32be1e95cbd4d4230c66ccba8a953481237
Lucia Cabrera <cabrera.blanca@hotmail.com>
2022-01-27 17:34:43 -0300

    color picker

--- main.js
+++ main.js
@@ -19,12 +19,26 @@ botonesOperadores.forEach(boton =>{
 
 /*COLOR PICKER*/
 
+/*background*/
+
 window.addEventListener("load", () => {
     const colorItems = document.querySelectorAll('.color-item');
     colorItems.forEach(item =>{
         item.addEventListener('click', function(){
             const idSelected = this.id;
             document.body.className = idSelected;
+        })
+    })
+})
+
+/*Calc color*/
+
+window.addEventListener("load", () => {
+    const colorItems = document.querySelectorAll('.color-item2');
+    colorItems.forEach(item =>{
+        item.addEventListener('click', function(){
+            const idSelected = this.id;
+            document.getElementById('calc').className= idSelected;
         })
     })
 })
